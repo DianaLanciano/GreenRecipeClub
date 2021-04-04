170f8b0f4f1ccb5cc1c14d9dc0542466ffd7b9d8
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-04 15:16:24 +0300

    added async methods

---
+++ app/src/main/java/com/example/greenrecipeclub/model/ModelFirebase.java
@@ -0,0 +1,9 @@
+package com.example.greenrecipeclub.model;
+
+public class ModelFirebase {
+    public static void addRecipe(Recipe recipe, Model.Listener<Boolean> listener) {
+    }
+
+    public static void deleteRecipe(Recipe recipe, Model.Listener<Boolean> listener) {
+    }
+}
