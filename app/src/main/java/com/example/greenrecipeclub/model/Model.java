72a0177dfc8883facd6cb1ed1ef3b6531cda4c84
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-04 11:40:40 +0300

    added Recipe class and Dao added connection to LocalDb

---
+++ app/src/main/java/com/example/greenrecipeclub/model/Model.java
@@ -0,0 +1,23 @@
+package com.example.greenrecipeclub.model;
+
+import java.util.List;
+
+public class Model {
+
+    public final static Model instance = new Model();
+    private  Model(){
+
+    }
+
+    List<Recipe> getAllRecipes(){
+        return null;
+    }
+    void addRecipe(Recipe recipe){
+
+    }
+    void updateRecipe(String id){
+
+    }
+
+
+}
