5067bcfe81f2f56ef78797a8401ab4442e298537
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 19:59:09 +0300

    recycler view waiting for DB

--- app/src/main/java/com/example/greenrecipeclub/fragments/ListOfRecipesFragment.java
+++ app/src/main/java/com/example/greenrecipeclub/fragments/ListOfRecipesFragment.java
@@ -2,6 +2,7 @@ package com.example.greenrecipeclub.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,7 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.example.greenrecipeclub.R;
 
@@ -42,4 +42,47 @@ public class ListOfRecipesFragment extends Fragment {
 
 
     }
+
+
+   static class RecipeViewHolder extends RecyclerView.ViewHolder{
+        public RecipeViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+        }
+    }
+
+    interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+
+    public class MyAdapter extends RecyclerView.Adapter<RecipeViewHolder>{
+
+        private OnItemClickListener listener;
+        void setOnClickListener(OnItemClickListener listener){ this.listener=listener; }
+
+
+        @NonNull
+        @Override
+        public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.recipe_list_row, parent, false);
+            RecipeViewHolder recipeViewHolder = new RecipeViewHolder(view, listener);
+            return recipeViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+
+
 }
+
+
