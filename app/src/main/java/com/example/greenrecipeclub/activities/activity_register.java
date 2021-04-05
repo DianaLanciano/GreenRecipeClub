82ba4db9f90ca0b7e085096b313228aa824554e7
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 17:51:57 +0300

    working on register avtivity

---
+++ app/src/main/java/com/example/greenrecipeclub/activities/activity_register.java
@@ -0,0 +1,16 @@
+package com.example.greenrecipeclub.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.greenrecipeclub.R;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
