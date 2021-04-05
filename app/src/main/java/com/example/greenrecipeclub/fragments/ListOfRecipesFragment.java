19181673016a9a368298ca7be5fcbffca7352229
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 18:54:02 +0300

    added a recycler view

--- app/src/main/java/com/example/greenrecipeclub/fragments/ListOfRecipesFragment.java
+++ app/src/main/java/com/example/greenrecipeclub/fragments/ListOfRecipesFragment.java
@@ -3,64 +3,43 @@ package com.example.greenrecipeclub.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.greenrecipeclub.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ListOfRecipesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ListOfRecipesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView categoryList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ListOfRecipesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListOfRecipesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListOfRecipesFragment newInstance(String param1, String param2) {
-        ListOfRecipesFragment fragment = new ListOfRecipesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_of_recipes, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_of_recipes, container, false);
+
+        categoryList = view.findViewById(R.id.RecyclerlistOfrecipes_recipes_screen);
+        categoryList.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        categoryList.setLayoutManager(layoutManager);
+
+
+
+        return view;
+
+
+
     }
 }
