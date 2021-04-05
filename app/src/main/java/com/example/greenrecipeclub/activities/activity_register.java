b7d3dcf936c22b2873608428ca70a425ce46eae0
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 22:44:45 +0300

    upload image process

--- app/src/main/java/com/example/greenrecipeclub/activities/activity_register.java
+++ app/src/main/java/com/example/greenrecipeclub/activities/activity_register.java
@@ -6,23 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.greenrecipeclub.R;
 
 public class activity_register extends AppCompatActivity {
 
     Button loginBtn;
+    EditText userName = findViewById(R.id.usernameInput_register_screen);
+    EditText password = findViewById(R.id.passwordInput_register_screen);
+    EditText email = findViewById(R.id.emailInput_register_screen);
+    ImageButton userImage = findViewById(R.id.imageInput_register_screen);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         loginBtn = findViewById(R.id.loginButton_register_screen);
-        loginBtn.setOnClickListener(new View.OnClickListener(){
+        loginBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 toLoginPage();
+            }
+        });
+
+
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
