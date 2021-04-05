e5562f804bc7d206164d6f98cb48dc23c10886f4
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 18:32:15 +0300

    added login and register design

--- app/src/main/java/com/example/greenrecipeclub/activities/activity_login.java
+++ app/src/main/java/com/example/greenrecipeclub/activities/activity_login.java
@@ -2,15 +2,40 @@ package com.example.greenrecipeclub.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.greenrecipeclub.R;
 
 public class activity_login extends AppCompatActivity {
 
+
+    EditText emailInput;
+    EditText passwordInput;
+    Button loginBtn;
+    Button registerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerBtn = findViewById(R.id.registerButton_login_screen);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                toRegisterPage();
+            }
+        });
+
+    }
+
+    private void toRegisterPage() {
+        Intent intent = new Intent(this, activity_register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/greenrecipeclub/activities/activity_register.java
+++ app/src/main/java/com/example/greenrecipeclub/activities/activity_register.java
@@ -2,15 +2,33 @@ package com.example.greenrecipeclub.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.greenrecipeclub.R;
 
 public class activity_register extends AppCompatActivity {
 
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        loginBtn = findViewById(R.id.loginButton_register_screen);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                toLoginPage();
+            }
+        });
+    }
+
+    private void toLoginPage() {
+        Intent intent = new Intent(this, activity_login.class);
+        startActivity(intent);
     }
 }
