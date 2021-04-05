9cabbdb9b0186f1d072159788bdc3c3edb3d577c
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 22:44:50 +0300

    upload image process

---
+++ app/src/main/java/com/example/greenrecipeclub/model/Utils.java
@@ -0,0 +1,29 @@
+package com.example.greenrecipeclub.model;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import com.example.greenrecipeclub.MyApplication;
+
+public class Utils {
+
+    static int REQ_CODE = 1;
+
+    public static void uploadImgFromGallery(Activity sender)
+    {
+        try
+        {
+            Intent openGalleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            openGalleryIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+
+            sender.startActivityForResult(openGalleryIntent, REQ_CODE);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MyApplication.context,  e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
