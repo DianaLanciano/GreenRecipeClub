a63e08f654d6ce79027fb194d0d1a72c4ed121d4
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-05 17:00:34 +0300

    added start and login

--- app/src/main/java/com/example/greenrecipeclub/activities/MainActivity.java
+++ app/src/main/java/com/example/greenrecipeclub/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.greenrecipeclub;
+package com.example.greenrecipeclub.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -7,6 +7,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.greenrecipeclub.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/example/greenrecipeclub/activities/activity_start.java
@@ -0,0 +1,45 @@
+package com.example.greenrecipeclub.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.greenrecipeclub.R;
+
+
+public class activity_start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Thread(){
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                } finally {
+                    toLoginPage();
+                }
+            }
+        }.start();
+        
+        
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
+
+    private void toLoginPage() {
+        Intent intent = new Intent(this, activity_login.class);
+        startActivity(intent);
+    }
+}
