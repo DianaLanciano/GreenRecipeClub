376952f7d9dde89e6d8cdcde2f42ece6fbdc7b79
Diana Lanciano <isakovdiana1@gmail.com>
2021-04-07 17:25:48 +0300

    added design to AddNewRecipeFragment

--- app/src/main/java/com/example/greenrecipeclub/fragments/AddNewRecipeFragment.java
+++ app/src/main/java/com/example/greenrecipeclub/fragments/AddNewRecipeFragment.java
@@ -10,52 +10,14 @@ import android.view.ViewGroup;
 
 import com.example.greenrecipeclub.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddNewRecipeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AddNewRecipeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AddNewRecipeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddNewRecipeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddNewRecipeFragment newInstance(String param1, String param2) {
-        AddNewRecipeFragment fragment = new AddNewRecipeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
